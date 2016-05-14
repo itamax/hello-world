2dfa78c1aca656ae4a0000c8e24635bb01cee9c1
Bruno Alexandre <brunoxalexandre@gmail.com>
2016-05-14 00:27:44 -0300

    Add files via upload

---
+++ apresformu.pas
@@ -0,0 +1,30 @@
+unit apresformu;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls, DrLabel, jpeg;
+
+type
+  Tapresform = class(TForm)
+    DRLabel3: TDRLabel;
+    Shape1: TShape;
+    Image2: TImage;
+    Image3: TImage;
+    Label6: TLabel;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  apresform: Tapresform;
+
+implementation
+
+{$R *.dfm}
+
+end.
